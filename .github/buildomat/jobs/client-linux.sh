b51ae0301385404fbb6944d0f1ecf401fe8dd1c8
Alex Plotnick <alex@oxidecomputer.com>
2026-08-10 21:17:14 +0000

    Build client binaries with buildomat
    A helios builder compiles the client natively, which cross-compiling
    cannot do cheaply: the crypto stack needs a C toolchain for the
    target. Both jobs build with the permslip feature and publish the
    binary, so support engineers can fetch a working client for either
    platform from any commit.

---
+++ .github/buildomat/jobs/client-linux.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#: name = "client (linux)"
+#: variety = "basic"
+#: target = "ubuntu-22.04"
+#: rust_toolchain = true
+#: output_rules = ["=/work/sush"]
+#: access_repos = ["oxidecomputer/permission-slip"]
+#:
+#: [[publish]]
+#: series = "linux"
+#: name = "sush"
+#: from_output = "/work/sush"
+
+set -o errexit
+set -o pipefail
+set -o xtrace
+
+cargo --version
+rustc --version
+
+# Cargo's builtin fetch does not read buildomat's netrc token.
+export CARGO_NET_GIT_FETCH_WITH_CLI=true
+export CARGO_INCREMENTAL=0
+
+mkdir -p /work
+
+cargo build --release --locked --package sush-client --features permslip
+cp target/release/sush /work/sush
